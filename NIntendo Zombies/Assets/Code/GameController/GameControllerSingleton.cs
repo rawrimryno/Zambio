c7fb4e43d70c76d917248543501117281a46bc03
Todd <toddmoulder88@gmail.com>
2016-02-05 21:06:30 -0600

    Abilities from PowerUps model in place, Identified by name, could be an issue when we make multiple and they auto-generate a number at the end.  Concept seems good though, could extend to tags if need be.

---
+++ NIntendo Zombies/Assets/Code/GameController/GameControllerSingleton.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControllerSingleton : ScriptableObject
+{
+    private static GameControllerSingleton Instance;
+
+    public struct PowerUp
+    {
+        
+        public Sprite sp;
+        public string alias;
+        public int Id;
+        public string desc;
+    }
+
+    public static GameControllerSingleton get()
+    {
+        if (Instance == null)
+        {
+            Instance = ScriptableObject.CreateInstance<GameControllerSingleton>();
+            Instance.Start();
+        }
+
+        return Instance;
+    }
+    // Use this for initialization
+    public void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    // Main Game Logic Flow, Coroutine to do time dependent things.
+    public void Update()
+    {
+
+
+    }
+
+    public PowerUp getPowerUp( string name )
+    {
+        PowerUp reward;
+        reward = new PowerUp();
+        // I think a dictionary is what we need here, but I need to look into it more,
+        // so here is something that should work, albeit outdated
+        if ( name == "Fire")
+        {
+            reward.desc = "The fire power-up!  It does firey things....";
+            reward.Id = 1;
+            reward.sp = null;
+            reward.alias = "Fire";
+        }
+        else if ( name == "SuperJump") {
+            reward.desc = "Allows you to jump a higher!";
+            reward.Id = 2;
+            reward.sp = null;
+            reward.alias = "SuperJump";
+        }
+        else
+        {
+            reward.desc = "Default";
+            reward.sp = null;
+            reward.alias = "Default";
+            reward.Id = -1;
+        }
+        return reward;
+    }
+}
---
+++ NIntendo Zombies/Assets/Code/Player/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class PlayerController : MonoBehaviour {
+    Rigidbody rb;
+    GameControllerSingleton gc;
+
+    private List<GameControllerSingleton.PowerUp> PowerUps;
+
+    private GameControllerSingleton.PowerUp tempPowerUp;
+
+    private bool hasFire, hasJump;
+
+    public float forwardScalar, jumpScalar;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        gc = GameControllerSingleton.get();
+        if (forwardScalar <= 0)
+        {
+            forwardScalar = 3;
+        }
+        if (jumpScalar <= 0)
+        {
+            jumpScalar = 3;
+        }
+        PowerUps = new List<GameControllerSingleton.PowerUp>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(forwardScalar * Vector3.forward);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(-forwardScalar * Vector3.forward);
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (Math.Abs(rb.velocity.y) < 0.1)
+            {
+                rb.AddForce( (hasJump ? 2 : 1) * jumpScalar * Vector3.up);
+            }
+        }
+	}
+
+    void OnCollisionEnter( Collision cInfo )
+    {
+        if (cInfo.collider.CompareTag("PowerUp"))
+        {
+            tempPowerUp = gc.getPowerUp(cInfo.gameObject.name);
+            PowerUps.Add(tempPowerUp);
+            setPowerUp(tempPowerUp);
+            tempPowerUp.Id = -1; // Not valid
+        }
+    }
+
+    void setPowerUp( GameControllerSingleton.PowerUp check)
+    {
+        switch( check.Id ) {
+            case 1:
+                hasFire = true; break;
+            case 2:
+                hasJump = true; break;
+            default:
+                Debug.Log("SetPowerUp Found something weird."); break;
+        }
+    }
+}
