c25f8dbdc9082cca1e93446615b9a695d1973690
Todd <toddmoulder88@gmail.com>
2016-02-05 21:06:30 -0600

    Abilities from PowerUps model in place, Identified by name, could be an issue when we make multiple and they auto-generate a number at the end.  Concept seems good though, could extend to tags if need be.

---
+++ NIntendo Zombies/Assets/Code/GameController/GameController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    GameControllerSingleton gameController;
+
+	// Use this for initialization
+	 void Start () {
+        gameController = GameControllerSingleton.get();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameController.Update();
+	}
+}
