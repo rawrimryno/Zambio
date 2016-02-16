480917e28379d01b8ca2b5290fa76f62f0b67810
Ryan Ngo <rawrimryno@gmail.com>
2016-02-16 00:12:32 -0600

    MOVEMENT AND GUNS !!!!!!!!!!!!!!!!!!!!!
    movement is very slightly bugged...guns work. still need a good bit of
    tweaking but hey it works!

---
+++ NIntendo Zombies/Assets/Code/Player/PlayerShoot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour {
+
+    public Rigidbody projectile;
+    public float bulletSpeed;
+
+	// Use this for initialization
+	void Start () {
+	    if (bulletSpeed == 0)
+        {
+            bulletSpeed = 20.0f;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Rigidbody clone;
+            clone = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+            clone.velocity = transform.TransformDirection(Vector3.forward * bulletSpeed);
+        }
+	}
+}
